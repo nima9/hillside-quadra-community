b96e6de8eb35485c00b5199aed1f6ad2075593f5
Nima Mohajeri <nima.mohajeri@buyerlink.com>
2025-02-12 18:09:27 -0800

    schema inti

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,5 +2,6 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	age: integer('age'),
+	name: text('name')
 });
